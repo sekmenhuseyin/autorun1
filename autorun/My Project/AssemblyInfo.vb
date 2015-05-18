22fc91e5c9b8a15657c7a8e2a0406668d201f727
huseyin <hus_@yahoo.com>
2015-05-18 13:04:10 +0300

    18.05.2015: working first commit

---
+++ autorun/My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("autorun")> 
+<Assembly: AssemblyDescription("autorun")> 
+<Assembly: AssemblyCompany("Sürat")> 
+<Assembly: AssemblyProduct("autorun")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("sürat")> 
+
+<Assembly: ComVisible(False)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2af649fa-eeb5-4a69-9534-b9c404f4b4ed")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("tr-TR")> 
---
+++ autorun/main.vb
@@ -0,0 +1,40 @@
+﻿Imports System.IO
+Imports System.Xml
+
+Module main
+    Dim isLibrary As Boolean = False : Dim Yayinevi, KitapIsmi As String
+    Sub Main()
+        Dim klasor As New IO.DirectoryInfo(My.Application.Info.DirectoryPath) : Dim Dosya As IO.DirectoryInfo() = klasor.GetDirectories() : Dim FileNames As IO.DirectoryInfo
+        Dim html As String = "" : Dim FolderCount As Integer = 0
+        For Each FileNames In Dosya
+            If File.Exists(FileNames.FullName & "\atu.exe") And File.Exists(FileNames.FullName & "\Book\Data\info.xml") Then
+                Dim xmldoc As New XmlDocument : Dim xmlnode As XmlNodeList
+                Dim fs As New FileStream(FileNames.FullName & "\Book\Data\info.xml", FileMode.Open, FileAccess.Read)
+                xmldoc.Load(fs)
+                xmlnode = xmldoc.GetElementsByTagName("info")
+                Yayinevi = xmlnode(0).Item("YayineviAdi").InnerText.Replace(" Yayınları", "")
+                KitapIsmi = xmlnode(0).Item("KitapIsmi").InnerText
+                html &= WriteBook(FileNames.FullName & "\atu.exe", Yayinevi & " " & KitapIsmi, FileNames.Name)
+                Console.WriteLine(FileNames.Name & ": " & Yayinevi & " " & KitapIsmi)
+                FolderCount += 1
+            End If
+        Next
+        If FolderCount > 0 Then
+            html = "<!DOCTYPE html><html><head><meta charset=""utf-8""><title>AutoRun</title>" &
+                    "<HTA:APPLICATION ID=""oMyApp"" APPLICATIONNAME = ""Application Executer"" BORDER = ""no"" CAPTION = ""yes"" ShowInTaskbar = ""yes"" SINGLEINSTANCE = ""yes"" SYSMENU = ""yes"" Scroll = ""no"" WINDOWSTATE=""normal"" MAXIMIZEBUTTON=""no"" SELECTION=""no"" ICON=""Autorun.ico"" />" &
+                    "<style>body{background:url(Background.jpg) no-repeat center center fixed;background-size:cover;min-height:500px;padding:200px 0 0 0;margin:0;}h3{max-width:150px;margin:5px;}" &
+                    "#panel{width:" & 300 * FolderCount & "px;margin:0 auto;}.cerceve{width:280px;height:500px;padding:0;float:left;margin:0 10px;text-align:center;background:url(Background.png) no-repeat;background-size:100%;}img{border:0;}</style>" &
+                    "</head><body><div id=""panel"">" & html & "</div>"
+            html &= "<script type=""text/javascript"" language=""javascript"">window.resizeTo(1024,768);window.onresize=function(){window.resizeTo(1024,768);};function RunFile(src) {WshShell = new ActiveXObject(""WScript.Shell"");WshShell.Run(src, 1, false);window.close();}</script>"
+            html &= "</body></html>"
+            File.Delete(My.Application.Info.DirectoryPath & "\index.hta")
+            Dim objWriter As New System.IO.StreamWriter(My.Application.Info.DirectoryPath & "\index.hta")
+            objWriter.Write(html)
+            objWriter.Close()
+            Shell("explorer.exe " & My.Application.Info.DirectoryPath & "\index.hta")
+        End If
+    End Sub
+    Private Function WriteBook(ExeFile As String, title As String, foldername As String) As String
+        Return "<div class=""cerceve""><a href=""#"" onclick=""RunFile('file:///" & ExeFile.Replace("\", "/") & "');""><img src=""" & foldername & "/Book/Images/Pages/Thumbs/Sayfa1.etf"" height=""280"" /></a><h3>" & title & "<h3></div>"
+    End Function
+End Module
