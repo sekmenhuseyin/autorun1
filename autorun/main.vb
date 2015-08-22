b4b7d0a4b7809ecbeea1bb747078616d7391ec69
huseyin <hus_as@yahoo.com>
2015-08-22 12:28:10 +0300

    21.08.2015: settings & update option added

--- autorun/main.vb
+++ autorun/main.vb
@@ -1,37 +1,29 @@
 ﻿Imports System.IO
+Imports System.Net
 Imports System.Xml
 
 Module main
     Dim lstSinif As List(Of String) = New List(Of String) : Dim lstBrans As List(Of String) = New List(Of String) : Dim lstTur As List(Of String) = New List(Of String)
     Dim lstSinifID As List(Of String) = New List(Of String) : Dim lstBransID As List(Of String) = New List(Of String) : Dim lstTurID As List(Of String) = New List(Of String)
-    Dim HtaAddress As String = My.Computer.FileSystem.SpecialDirectories.Temp & "\CX43TAD.hta"
-    Dim AppAddress As String = My.Application.Info.DirectoryPath
+    Dim HtaFolder As String = My.Computer.FileSystem.SpecialDirectories.CurrentUserApplicationData & "\Sürat\"
+    Dim AppAddress As String = My.Application.Info.DirectoryPath : Dim isler As List(Of Integer) = New List(Of Integer)
     Sub Main()
         Dim isLibrary As Boolean = True
         Dim xmldoc As New XmlDocument : Dim xmlnode As XmlNodeList
-        Dim No, Yayinevi, KitapIsmi, InternetSitesi, SonSayfa, Sinif(1), Tur(1), Brans(1), title, web, html As String : Dim i As Integer = 0
+        Dim No, Yayinevi, KitapIsmi, InternetSitesi, SonSayfa, Sinif(1), Tur(1), Brans(1), title, web, html, version As String : Dim i As Integer = 0
         html = "" : title = "" : web = ""
+        'control
+        If Directory.Exists(HtaFolder) = False Then Directory.CreateDirectory(HtaFolder)
+        If File.Exists(HtaFolder & "setting.ini") = False Then CreateSettings()
+        Dim CheckForUpdates As Boolean = AskUpdates()
         'search in directories
         Dim dirs As String() = Directory.GetDirectories(AppAddress)
         For Each Dir As String In dirs
             'eğer atu klasörü ise işleme devam et
             If File.Exists(Dir & "\atu.exe") And File.Exists(Dir & "\Book\Data\info.xml") Then
                 Dim tmp() As String = Dir.Split("\") : Dim klasoryolu As String = tmp(UBound(tmp))
-                No = "" : Yayinevi = "" : KitapIsmi = "" : InternetSitesi = "" : SonSayfa = "" : Sinif(0) = "" : Tur(0) = "" : Brans(0) = "" : Sinif(1) = "" : Tur(1) = "" : Brans(1) = "" : i += 1
-                'eğer xmlde ilk satır indo değilse hata veriyordu
-                'bu yüzden infoda öncesini sildiriyorum
-                Dim objWriter As New StreamReader(Dir & "\Book\Data\info.xml")
-                Dim alltext = objWriter.ReadToEnd
-                Dim pos = InStr(alltext, "<info>")
-                If pos <> 1 Then
-                    objWriter.Close() : objWriter.Dispose() 'önce açık olanı kapat
-                    alltext = Mid(alltext, pos) 'info yazısından itibaren yazdır
-                    Dim objWriter2 As New StreamWriter(Dir & "\Book\Data\info.xml")
-                    objWriter2.Write(alltext)
-                    objWriter2.Close() : objWriter2.Dispose()
-                Else
-                    objWriter.Close() : objWriter.Dispose()
-                End If
+                No = "" : Yayinevi = "" : KitapIsmi = "" : InternetSitesi = "" : SonSayfa = "" : Sinif(0) = "" : Tur(0) = "" : Brans(0) = "" : Sinif(1) = "" : Tur(1) = "" : Brans(1) = "" : version = "1.0" : i += 1
+                ClearXml(Dir)
                 'her kitap için htmlde bir satır oluştur
                 'xmlden okuma bmlünü
                 Dim fs As New FileStream(Dir & "\Book\Data\info.xml", FileMode.Open, FileAccess.Read)
@@ -49,8 +41,9 @@ Module main
                 If IsNothing(xmlnode(0).Item("TurAd")) = False Then Tur(1) = xmlnode(0).Item("TurAd").InnerText
                 If IsNothing(xmlnode(0).Item("Brans")) = False Then Brans(0) = xmlnode(0).Item("Brans").InnerText
                 If IsNothing(xmlnode(0).Item("BransAd")) = False Then Brans(1) = xmlnode(0).Item("BransAd").InnerText
+                If IsNothing(xmlnode(0).Item("Surum")) = False Then version = xmlnode(0).Item("Surum").InnerText
                 'genel bilgiler
-                If No = "" Then No = i
+                If No = "" Then No = i Else writeIni(HtaFolder & "setting.ini", "Versions", No, version) : isler.Add(No) 'eğer işler no varsa sürümünü yaz
                 If title = "" Then title = Yayinevi
                 If web = "" Then web = InternetSitesi
                 If lstSinif.Contains(Sinif(1)) = False Then lstSinif.Add(Sinif(1)) : lstSinifID.Add(Sinif(0))
@@ -61,7 +54,7 @@ Module main
                     If html <> "" Then html &= ","
                     html &= WriteBook2(No, KitapIsmi, klasoryolu, Dir & "\atu.exe", SonSayfa, Tur(1), Tur(0), Brans(1), Brans(0), Sinif(1), Sinif(0))
                 Else
-                    html &= WriteBook(Dir & "\atu.exe", Yayinevi.Replace(" Yayınları", "") & " " & KitapIsmi, klasoryolu)
+                    html &= WriteBook(Dir & "\atu.exe", Yayinevi.Replace(" Yayınları", "").Replace(" Yayın", "").Replace(" Publishing", "") & " " & KitapIsmi, klasoryolu)
                 End If
                 'kapat
                 fs.Close() : fs.Dispose()
@@ -75,13 +68,15 @@ Module main
             'sayfayı yazdır
             html = WriteHta(isLibrary, True, genislik, title, web) & html & WriteHta(isLibrary, False)
             'hta yazdır
-            File.Delete(HtaAddress)
-            Dim objWriter As New System.IO.StreamWriter(HtaAddress)
+            File.Delete(HtaFolder & "Autorun.hta")
+            Dim objWriter As New StreamWriter(HtaFolder & "Autorun.hta")
             objWriter.Write(html)
             objWriter.Close()
             've başlat
-            Shell("explorer.exe " & HtaAddress)
+            Shell("explorer.exe " & HtaFolder & "Autorun.hta")
         End If
+        'güncelleme zamanı
+        If CheckForUpdates = True Then CheckUpdate()
     End Sub
     'az kitap için sadece resmi ve adı yazılıyor
     Private Function WriteBook(ExeFile As String, title As String, foldername As String) As String
@@ -163,4 +158,71 @@ Module main
         End If
         Return html
     End Function
+    'Info.xml kontrol
+    Private Sub ClearXml(dir As String)
+        'eğer xmlde ilk satır indo değilse hata veriyordu
+        'bu yüzden infoda öncesini sildiriyorum
+        Dim objWriter As New StreamReader(dir & "\Book\Data\info.xml")
+        Dim alltext = objWriter.ReadToEnd
+        Dim pos = InStr(alltext, "<info>")
+        If pos <> 1 Then
+            objWriter.Close() : objWriter.Dispose() 'önce açık olanı kapat
+            alltext = Mid(alltext, pos) 'info yazısından itibaren yazdır
+            Dim objWriter2 As New StreamWriter(dir & "\Book\Data\info.xml")
+            objWriter2.Write(alltext)
+            objWriter2.Close() : objWriter2.Dispose()
+        Else
+            objWriter.Close() : objWriter.Dispose()
+        End If
+    End Sub
+    'create default settings file
+    Private Sub CreateSettings()
+        writeIni(HtaFolder & "setting.ini", "General", "LastCheck", Now())
+        writeIni(HtaFolder & "setting.ini", "General", "CountOfNos", "0")
+    End Sub
+    'güncelleme yapsın mı
+    Private Function AskUpdates() As Boolean
+        Dim CheckForUpdates As Boolean = False
+        Dim LastCheck As DateTime = Convert.ToDateTime(ReadIni(HtaFolder & "setting.ini", "General", "LastCheck"))
+        Dim CountOfNos As Int16 = Convert.ToInt16(ReadIni(HtaFolder & "setting.ini", "General", "CountOfNos"))
+        If LastCheck < Now().AddDays(-30) Then
+            If MsgBox("Güncellemeleri kontrol etmek ister misiniz?", MsgBoxStyle.Question + MsgBoxStyle.YesNo + MsgBoxStyle.MsgBoxSetForeground, "Autorun") = MsgBoxResult.Yes Then
+                CheckForUpdates = True
+                writeIni(HtaFolder & "setting.ini", "General", "CountOfNos", "0")
+            Else
+                writeIni(HtaFolder & "setting.ini", "General", "CountOfNos", CountOfNos + 1)
+                If CountOfNos > 3 Then writeIni(HtaFolder & "setting.ini", "General", "LastCheck", Now().AddMonths(6)) : writeIni(HtaFolder & "setting.ini", "General", "CountOfNos", "0")
+            End If
+        End If
+        Return CheckForUpdates
+    End Function
+    'ne kadar güncelleme var?
+    Private Sub CheckUpdate()
+        Dim webClient As New WebClient : Dim KitapSayisi As Integer = 0 : Dim version As String
+        'autorun.exe güncelleme kontrol
+        version = My.Application.Info.Version.Major & "." & My.Application.Info.Version.Minor
+        Dim updateAutorun As String = webClient.DownloadString("http://kaynakkatalog.com/API/CheckUpdates").Replace("""", "")
+        If updateAutorun <> version Then updateAutorun = "yes"
+        'atu.exe güncelleme kontrol
+        'tüm kitapları tek tek kontrol et
+        For Each item In isler
+            version = ReadIni(HtaFolder & "setting.ini", "General", item) : If version = "" Then version = "1.0"
+            Dim updateKitap As String = webClient.DownloadString("http://kaynakkatalog.com/API/CheckUpdates/" & item).Replace("""", "") : If updateKitap = "" Then updateKitap = "1.0"
+            If updateKitap <> version Then KitapSayisi += 1
+        Next
+        Dim tmp As String = ""
+        If updateAutorun = "yes" Then tmp = "Program güncellenecek." & vbCrLf
+        If KitapSayisi > 0 Then tmp &= KitapSayisi & " adet kitap güncellenecek."
+        If tmp <> "" Then
+            If MsgBox(tmp, MsgBoxStyle.YesNo + MsgBoxStyle.Question + MsgBoxStyle.MsgBoxSetForeground, "Autorun") = MsgBoxResult.Yes Then
+                Update()
+                writeIni(HtaFolder & "setting.ini", "General", "LastCheck", Now())
+            End If
+        End If
+        Exit Sub
+    End Sub
+    'güncelleme
+    Private Sub Update()
+
+    End Sub
 End Module
